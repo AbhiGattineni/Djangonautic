730ed86e2d965e8395b0d3e16064f73cf5e9f65b
AbhiGattineni <abhishekgattineni@gmail.com>
2022-06-07 17:24:30 -0700

    lesson 25 completed

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -3,12 +3,15 @@ from django.urls import path
 from .import views
 from django.conf.urls import include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.homepage),
+    path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('articles/', include('articles.urls')),
 
     # path(r'^admin/', admin.site.urls),
     # path(r'^about/$', views.about),
@@ -16,3 +19,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
